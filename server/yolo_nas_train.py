d9db36f14b38fb01688b949c8e41b4d8e184dc17
asdwoo123 <39832375+asdwoo123@users.noreply.github.com>
2023-12-27 16:18:42 +0900

    Update yolo_nas_train.py

--- server/yolo_nas_train.py
+++ server/yolo_nas_train.py
@@ -11,6 +11,7 @@ from super_gradients.training.metrics import (
 )
 from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
 import torch
+import threading
 
 
 def callback_function(epoch, step, metrics):
@@ -69,7 +70,7 @@ class YoloNasTrain():
             }
         )
 
-        train_params = {
+        self.train_params = {
             'silent_mode': False,
             "average_best_models":True,
             "warmup_mode": "linear_epoch_step",
@@ -133,10 +134,130 @@ class YoloNasTrain():
         
         trainer.train(
             model=model, 
-            training_params=train_params, 
+            training_params=self.train_params, 
             train_loader=train_data, 
             valid_loader=val_data
         )
 
 
+class YoloNasThread(threading.Thread):
+    def __init__(self, dataset_dir, checkpoints_dir, device, EPOCHS, BATCH_SIZE, WORKERS, classes):
+        super().__init__()
+        self.model_to_train = 'yolo_nas_s'
+        self.CHECKPOINT_DIR = checkpoints_dir
+        self.ROOT_DIR = dataset_dir
+        self.DEVICE = device
+        self.train_imgs_dir = 'train/images'
+        self.train_labels_dir = 'train/labels'
+        self.val_imgs_dir = 'valid/images'
+        self.val_labels_dir = 'valid/labels'
+        self.test_imgs_dir = 'test/images'
+        self.test_labels_dir = 'test/labels'
 
+        dataset_params = {
+            'data_dir': self.ROOT_DIR,
+            'train_imgs_dir': self.train_imgs_dir,
+            'train_labels_dir': self.train_labels_dir,
+            'val_imgs_dir': self.val_imgs_dir,
+            'val_labels_dir': self.val_labels_dir,
+            'test_imgs_dir': self.test_imgs_dir,
+            'test_labels_dir': self.test_labels_dir,
+            'classes': classes
+        }
+        
+        self.train_data = coco_detection_yolo_format_train(
+            dataset_params={
+                'data_dir': dataset_params['data_dir'],
+                'images_dir': dataset_params['train_imgs_dir'],
+                'labels_dir': dataset_params['train_labels_dir'],
+                'classes': dataset_params['classes']
+            },
+            dataloader_params={
+                'batch_size': BATCH_SIZE,
+                'num_workers': WORKERS
+            }
+        )
+
+        self.val_data = coco_detection_yolo_format_val(
+            dataset_params={
+                'data_dir': dataset_params['data_dir'],
+                'images_dir': dataset_params['val_imgs_dir'],
+                'labels_dir': dataset_params['val_labels_dir'],
+                'classes': dataset_params['classes']
+            },
+            dataloader_params={
+                'batch_size': BATCH_SIZE,
+                'num_workers': WORKERS
+            }
+        )
+
+        self.train_params = {
+            'silent_mode': False,
+            "average_best_models":True,
+            "warmup_mode": "linear_epoch_step",
+            "warmup_initial_lr": 1e-6,
+            "lr_warmup_epochs": 3,
+            "initial_lr": 5e-4,
+            "lr_mode": "cosine",
+            "callback_function": callback_function,
+            "cosine_final_lr_ratio": 0.1,
+            "optimizer": "Adam",
+            "optimizer_params": {"weight_decay": 0.0001},
+            "zero_weight_decay_on_bias_and_bn": True,
+            "ema": True,
+            "ema_params": {"decay": 0.9, "decay_type": "threshold"},
+            "max_epochs": EPOCHS,
+            "mixed_precision": True,
+            "loss": PPYoloELoss(
+                use_static_assigner=False,
+                num_classes=len(dataset_params['classes']),
+                reg_max=16
+            ),
+            "valid_metrics_list": [
+                DetectionMetrics_050(
+                    score_thres=0.1,
+                    top_k_predictions=300,
+                    num_cls=len(dataset_params['classes']),
+                    normalize_targets=True,
+                    post_prediction_callback=PPYoloEPostPredictionCallback(
+                        score_threshold=0.01,
+                        nms_top_k=1000,
+                        max_predictions=300,
+                        nms_threshold=0.7
+                    )
+                ),
+                DetectionMetrics_050_095(
+                    score_thres=0.1,
+                    top_k_predictions=300,
+                    num_cls=len(dataset_params['classes']),
+                    normalize_targets=True,
+                    post_prediction_callback=PPYoloEPostPredictionCallback(
+                        score_threshold=0.01,
+                        nms_top_k=1000,
+                        max_predictions=300,
+                        nms_threshold=0.7
+                    )
+                )
+            ],
+            "metric_to_watch": 'mAP@0.50:0.95'
+        }
+
+        self.trainer = Trainer(
+            experiment_name=self.model_to_train,
+            ckpt_root_dir=self.CHECKPOINT_DIR
+        )
+
+        self.model = models.get(
+            self.model_to_train, 
+            num_classes=len(dataset_params['classes']), 
+            pretrained_weights="coco"
+        ).to(self.DEVICE)
+        
+
+    def run(self):
+        self.trainer.train(
+            model=self.model, 
+            training_params=self.train_params, 
+            train_loader=self.train_data, 
+            valid_loader=self.val_data
+        )
